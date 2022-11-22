20d8555c8e4e34a6a97b8bab74a77bf53847a694
deezitheviper <amuhdavid@gmail.com>
2022-11-22 10:01:00 +0100

    layout and navbar

--- ecomm_nextjs/components/Footer.js
+++ ecomm_nextjs/components/Footer.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {AiFillGithub} from 'react-icons/ai';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+        <p> 2022 xOculus All rights reserved </p>
+        <p className='icons'>
+        <AiFillGithub/>
+        </p><Link href='https://github.com/deezitheviper'><small>deezitheviper</small></Link>
+    </div>
   )
 }
 
--- ecomm_nextjs/components/Navbar.js
+++ ecomm_nextjs/components/Navbar.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import {ImCart} from 'react-icons/im';
+import Image from 'next/image'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>
+       <Image src='/logo.png' alt='' width="25" height="16" />
+       {" "}xOculus
+        </Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick="">
+          <ImCart/>
+          <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
