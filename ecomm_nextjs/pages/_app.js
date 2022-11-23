c42dfd3fdb74a2706d0bdb57699d7c1d6b148c8b
deezitheviper <amuhdavid@gmail.com>
2022-11-24 00:01:37 +0100

    added loader spinner to routes

--- ecomm_nextjs/pages/_app.js
+++ ecomm_nextjs/pages/_app.js
@@ -1,13 +1,50 @@
 import '../styles/globals.css';
+import { useState, useEffect } from 'react';
+import {useRouter} from 'next/router';
 import Layout from '../components/Layout';
 import {StateContext} from '../context/stateContext';
 import {Toaster} from 'react-hot-toast';
+import {InfinitySpin} from 'react-loader-spinner';
+
+
+
+
+
+function Loading(){
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = url => (url !== router.asPath ) && setLoading(true);
+    const handleComplete = url => (url === router.asPath) && setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+    
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+  }
+  })
+  return loading && (
+    <div className="spinner-wrapper">
+          <InfinitySpin  className="spinner"
+  width='200'
+  color="#1d66c1"
+/>
+    </div>
+
+  )
+}
 
 
 function MyApp({ Component, pageProps }) {
   return (
 <StateContext>  
   <Layout>
+    <Loading/>
     <Toaster/>
    <Component {...pageProps} />
   </Layout>
