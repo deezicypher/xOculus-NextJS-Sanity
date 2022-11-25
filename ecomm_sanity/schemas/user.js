3f37d3ab728097c227737ab74460593821e46da6
deezitheviper <amuhdavid@gmail.com>
2022-11-25 06:01:52 +0100

    user schema

---
+++ ecomm_sanity/schemas/user.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string',
+        },
+        {
+            name: 'address',
+            title: 'Shipping Address',
+            type: 'string',
+        },
+        {
+            name: 'password',
+            title: 'Password',
+            type: 'string',
+        },
+        {
+            name: 'isAdmin',
+            title: 'Is Admin',
+            type: 'boolean',
+        }
+    ]
+}
