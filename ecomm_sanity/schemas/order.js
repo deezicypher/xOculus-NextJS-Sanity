1be50eccecf69e260cd87defd8c40610f7391c48
deezitheviper <amuhdavid@gmail.com>
2022-11-26 22:02:50 +0100

    new schema

---
+++ ecomm_sanity/schemas/order.js
@@ -0,0 +1,86 @@
+export default {
+    name: 'order',
+    title:'Order',
+    type:'document',
+    fields:[
+        {
+            name: 'user',
+            title: 'User',
+            type: 'reference',
+            to:[{type: 'user'}],
+            options:{
+                disableNew: true,
+            }
+        },
+        {
+            name:'name',
+            title:'Full Name',
+            type:'string'
+        },
+        {
+            name:'totalPrice',
+            title:'Total Price',
+            type:'number'
+        },
+        {
+            name:'totalQuantity',
+            title:'Total Quantity',
+            type:'number'
+        },
+        {
+            name:'shippingAddress',
+            title:'Shipping Address',
+            type:'shippingAddress'
+        },
+       
+        {
+            name:'paymentMethod',
+            title:'Payment Method',
+            type:'string'
+        },
+        {
+            name:'paymentResult',
+            title:'Payment Result',
+            type:'paymentResult'
+        },
+        {
+            name:'orderItems',
+            title:'Order Items',
+            type:'array',
+            of:[
+                {
+                    title:'Order Item',
+                    type:'orderItem'
+                }
+            ]
+        },
+        {
+            name:'paid',
+            title:'Paid',
+            type:'boolean'
+        },
+        {
+            title:'Paid On',
+            name:'paidOn',
+            type:'datetime',
+        },
+        {
+            name:'delivered',
+            title:'Delivered',
+            type:'boolean'
+        },
+        
+        {
+            name:'deliveredOn',
+            title:'Delivered On',
+            type:'datetime',
+        },
+        {
+            name:'orderedOn',
+            title:'Ordered On',
+            type:'datetime'
+        }
+
+
+    ]
+}
---
+++ ecomm_sanity/schemas/shipAdress.js
@@ -0,0 +1,33 @@
+export default {
+    name:'shippingAddress',
+    title:'Shipping Address',
+    type:'object',
+    fields:[
+        {
+            name:'name',
+            title:'Name',
+            type:'string',
+        },
+        {
+            name:'address',
+            title:'Address',
+            type:'string',
+        },
+        {
+            name:'city',
+            title:'City',
+            type:'string',
+        },
+        {
+            name:'postalcode',
+            title:'Postal Code',
+            type:'string',
+        },
+        {
+            name:'country',
+            title:'Country',
+            type:'string'
+        }
+    ]
+
+}
