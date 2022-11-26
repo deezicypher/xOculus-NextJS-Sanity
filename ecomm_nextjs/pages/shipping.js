51f2ececd19916b668fb7c73b0388ed055a71730
deezitheviper <amuhdavid@gmail.com>
2022-11-26 05:59:16 +0100

    shipping page

---
+++ ecomm_nextjs/pages/shipping.js
@@ -0,0 +1,108 @@
+import React, { useEffect } from 'react'
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import Link from 'next/link';
+import { useStateContext } from '../context/stateContext';
+import { useRouter } from 'next/router';
+
+
+const shipping = () => {
+    const {setShippingDetails,shippingDetails, user} = useStateContext();
+    const formSchema = yup.object().shape({
+        name: yup.string()
+        .required('Please enter your name'),
+        address: yup.string()
+        .required('Please enter your address'),
+        city: yup.string()
+        .required('Please enter your city'),
+        postalcode: yup.number()
+        .required('Please enter your city'),
+        country: yup.string()
+        .required('Please enter your city')
+
+      })
+
+    const formOptions = { resolver: yupResolver(formSchema) }
+    const { register, handleSubmit,setValue, formState: { errors } } = useForm(formOptions);
+    const router = useRouter();
+
+
+    const onSubmit = data => {
+        Cookies.set('shippingDetails',JSON.stringify(data));
+        setShippingDetails(data)
+    }
+
+    useEffect(() => {
+     
+       if (user){
+        setValue('name',shippingDetails?.name )
+        setValue('address',shippingDetails?.address)
+        setValue('city',shippingDetails?.city)
+        setValue('postalcode',shippingDetails?.postalcode)
+        setValue('country',shippingDetails?.country)
+       }else{
+       router.push('/Login?redirect=/shipping')
+       }
+
+    }, [router,setValue,shippingDetails,user])
+
+  return (
+    <div className='shipping-card'>
+    
+    <div className="auth-form shipping">
+ 
+    <div className='form-heading shipping'>
+      <h2>Shipping Details</h2>
+      <p >
+        to continue to checkout, fill shipping details
+      </p>
+    </div>
+
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+     
+        {errors.name && <p className='form-error' role="alert">{errors.name?.message}</p>}
+
+<input
+    placeholder='Full Name'
+  {...register("name")}
+/>
+
+{errors.address && <p className='form-error' role="alert">{errors.address?.message}</p>}
+
+<input
+    placeholder='Address'
+  {...register("address")}
+/>
+
+{errors.city && <p className='form-error' role="alert">{errors.city?.message}</p>}
+
+<input
+    placeholder='City'
+  {...register("city")}
+/>
+
+{errors.postalcode && <p className='form-error' role="alert">{errors.postalcode?.message}</p>}
+
+<input
+    placeholder='Postal Code'
+  {...register("postalcode")}
+/>
+
+{errors.country && <p className='form-error' role="alert">{errors.country?.message}</p>}
+
+<input
+    placeholder='Country'
+  {...register("country")}
+/>
+
+<input className='shipping-btn' type="submit" value="Pay With Stripe" />
+        </form>
+    </div>
+    
+    </div>
+  )
+}
+
+export default shipping
