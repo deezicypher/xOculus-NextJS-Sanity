2bc6dbc979f38678e789e2b165aa78dbe7d6b203
deezitheviper <amuhdavid@gmail.com>
2022-11-28 01:42:48 +0100

    user orders

---
+++ ecomm_nextjs/pages/api/profile/orders/index.js
@@ -0,0 +1,21 @@
+import { client } from "../../../../utils/client";
+import { isAuth } from "../../../../utils/Token";
+   
+ 
+export default async function (req, res){
+    const id = req.query.id;
+    if(req.method ==='GET'){
+        if(isAuth(req,res)){
+           
+            try{
+                const result = await client.fetch(`*[_type == "order" && user._id == "${id}"][0]`)
+               res.status(200).json(result);
+            }catch(err){
+                console.log(err)
+            }
+        }
+    }else{
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+}
